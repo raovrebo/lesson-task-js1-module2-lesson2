b1f8ef57ff06a29cfdc7e687b48033c92f9b701f
Roy Are Øvrebotten <69842808+raovrebo@users.noreply.github.com>
2021-02-10 14:23:13 +0100

    Finished lesson task

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let counter = 0;
+const button = document.querySelector("button");
+const countContainer = document.querySelector(".count");
+
+button.onclick = function () {
+    event.target.disabled = true;
+
+    counter = 0;
+
+    const intervalId = setInterval(function () {
+        counter++;
+        countContainer.innerHTML = counter;
+        console.log(countContainer.innerHTML = counter);
+
+        if(counter === 7) {
+            clearInterval(intervalId);
+            button.disabled = false;
+        }
+    }, 1000);
+}
+
